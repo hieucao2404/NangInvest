1a0d544fe683aceadc06f1cc09d62e11643f7266
Cao Vu Duc Hieu <hieucao240402@yahoo.com>
2026-03-16 15:47:40 +0700

    Clear Google client credentials in Constants.java
    Removed sensitive Google client ID and secret.

--- NangInvest/src/main/java/util/Constants.java
+++ NangInvest/src/main/java/util/Constants.java
@@ -10,8 +10,8 @@ package util;
  */
 public class Constants {
 
-    public static String GOOGLE_CLIENT_ID = "422211950963-r094spj3shieq20gkajosg6lplpppehv.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_CLIENT_ID = "";
+    public static String GOOGLE_CLIENT_SECRET = "";
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8081/NangInvest/login/google";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
